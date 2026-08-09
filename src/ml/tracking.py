a98070ab8cab92d2f1f6eabecfc6b201668a08af
Ander Cruz <anderjcruz@gmail.com>
2026-08-09 01:23:02 -0300

    feat: configure mlflow tracking uri

--- src/ml/tracking.py
+++ src/ml/tracking.py
@@ -1,3 +1,5 @@
+import os
+
 import mlflow
 
 
@@ -5,6 +7,16 @@ EXPERIMENT_NAME = "country-risk-prediction"
 
 
 def setup_mlflow():
+
+    tracking_uri = os.getenv(
+        "MLFLOW_TRACKING_URI"
+    )
+
+    if tracking_uri:
+
+        mlflow.set_tracking_uri(
+            tracking_uri
+        )
 
     mlflow.set_experiment(
         EXPERIMENT_NAME
