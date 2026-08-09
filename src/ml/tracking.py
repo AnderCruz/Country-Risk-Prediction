9e19eefa757e8751724b6f8272f27c8e56ad424a
Ander Cruz <anderjcruz@gmail.com>
2026-08-09 20:41:04 -0300

    feat: add mlflow tracking server with docker compose

--- src/ml/tracking.py
+++ src/ml/tracking.py
@@ -1,25 +1,12 @@
+# src/ml/tracking.py
 import os
-
 import mlflow
-
 
 EXPERIMENT_NAME = "country-risk-prediction"
 
-
 def setup_mlflow():
-
-    tracking_uri = os.getenv(
-        "MLFLOW_TRACKING_URI"
-    )
-
+    tracking_uri = os.getenv("MLFLOW_TRACKING_URI")
     if tracking_uri:
-
-        mlflow.set_tracking_uri(
-            tracking_uri
-        )
-
-    mlflow.set_experiment(
-        EXPERIMENT_NAME
-    )
-
+        mlflow.set_tracking_uri(tracking_uri)
+    mlflow.set_experiment(EXPERIMENT_NAME)
     return mlflow
